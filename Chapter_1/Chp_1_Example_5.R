a2d829da375056737b22920e2b7826669df97d7a
Simon Angoluan <88635723+scangoluan@users.noreply.github.com>
2022-08-14 21:07:39 -0400

    Chapter 1
    Added RScript and Annotated PDF for Chapter 1 Example 5

--- Chapter_1/Chp_1_Example_5.R
+++ Chapter_1/Chp_1_Example_5.R
@@ -21,10 +21,13 @@
 
 # Now, select this .csv file from your hard drive:
 mypath <- file.choose()
+
 # Now R knows the location of your file:
 mypath
+
 # The read.csv() command reads in .csv files:
 dataEx5 <- read.csv(mypath)
+
 # We can now view the file:
 dataEx5
 
@@ -37,8 +40,10 @@ dataEx5
 # I saved it on Gitub (see www.github.com)
 
 # If you have a .csv file sitting on the internet, and you know its url (web address), you can grab it from there.
+
 # For instance, the GoogleAnalytics file sits at the following address, which I enter into R:
 myurl <- 'https://raw.githubusercontent.com/artofstat/data/master/Chapter1/GoogleAnalyticsExample5.csv'
+
 # I can now load the data into R as before, using read.csv():
 dataEx5.remote <- read.csv(myurl)
 dataEx5.remote
